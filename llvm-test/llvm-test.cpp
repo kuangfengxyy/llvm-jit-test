f9344355cafd2a6754e8eb94631d5d428f24a66d
匡峰 <kuangfengxyy@gmail.com>
2015-07-04 10:49:48 +0800

    makefile

--- llvm-test/llvm-test.cpp
+++ llvm-test/llvm-test.cpp
@@ -14,13 +14,22 @@ int main()
 {
     LLVMContext context;
     string error;
-    Module *m = *parseBitcodeFile(MemoryBuffer::getFile("loop-test.bc").get()->getMemBufferRef(), context);
+    Module *m = *parseBitcodeFile(MemoryBuffer::getFile("loop-test.bc")->get()->getMemBufferRef(), context);
     // ExecutionEngine *ee = ExecutionEngine::create(m);
+    if (m == NULL) {
+        cout << "create Module fail" << endl;
+        exit(1);
+    }
     ExecutionEngine *ee = EngineBuilder(unique_ptr<Module>(m)).create();
     Function* func = ee->FindFunctionNamed("DefaultImplementation");
 
     typedef void (*PFN)();
     PFN pfn = reinterpret_cast<PFN>(ee->getPointerToFunction(func));
+    if (pfn == NULL) {
+        cout << "func is null"<<endl;
+        exit(1);
+    }
     pfn();
+    cout << "done pfn\n";
     delete ee;
 }
