490e8f9a9cae0372b72c459d16aa96182ec36a5b
kuangfengxyy <kuangfengxyy@gmail.com>
2015-07-31 03:47:23 +0800

    bench

--- simple-jit/codegen.cpp
+++ simple-jit/codegen.cpp
@@ -20,8 +20,35 @@
 #include <llvm/IR/IRBuilder.h>
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/Analysis/Passes.h>
+#include <llvm/Analysis/Passes.h>
+#include <llvm/ExecutionEngine/ExecutionEngine.h>
+#include <llvm/ExecutionEngine/MCJIT.h>
+#include <llvm/ExecutionEngine/SectionMemoryManager.h>
+#include <llvm/IR/DataLayout.h>
+#include <llvm/IR/DerivedTypes.h>
+#include <llvm/IR/IRBuilder.h>
+#include <llvm/IR/LLVMContext.h>
+#include <llvm/IR/LegacyPassManager.h>
+#include <llvm/IR/Module.h>
+#include <llvm/IR/Verifier.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Transforms/Scalar.h>
+#include <chrono>
+
 using namespace llvm;
-Function* codegenAggr(Module* m, LLVMContext& ctx) {
+using namespace std;
+enum TYPE {
+    UINT,
+    INT,
+    ULONG,
+    LONG,
+    DOUBLE,
+    FLOAT
+};
+typedef enum TYPE type_t;
+Function* codegenAggr(Module* m, LLVMContext& ctx, std::vector<type_t> types) {
     std::vector<Type*> args_types;
     args_types.push_back(Type::getInt8PtrTy(ctx));
     args_types.push_back(Type::getInt8PtrTy(ctx));
@@ -43,29 +70,113 @@ Function* codegenAggr(Module* m, LLVMContext& ctx) {
     }
     Value* row_arg = args[0];
     Value* torow_arg = args[1];
-    //BasicBlock* ent = BasicBlock::Create(ctx, "ent", fn);
-    //Constant* const_int = ConstantInt::get(Type::getInt64Ty(context()), (int64_t)ptr);
-    //Value* left_ptr_addr = builder.CreateAdd(row_arg, ConstantInt::getInt64Ty(ctx, table->_pos[i]);
-    //Value* right_ptr_addr = builder.CreateAdd(rowto_arg, ConstantInt::getInt64Ty(ctx, table->_pos[i]);
-    Value* pos = ConstantInt::get(Type::getInt64Ty(ctx), 8);
     IRBuilder<> builder(ctx);
     builder.SetInsertPoint(BB);
-    Value* value_ptr_int = builder.CreateAdd(pos, builder.CreatePtrToInt(row_arg, Type::getInt64Ty(ctx)));
-    Value* value_ptr = builder.CreateIntToPtr(value_ptr_int, Type::getInt64PtrTy(ctx));
-    Value* left = builder.CreateLoad(value_ptr);
-    Value* double_left = builder.CreateAdd(left, left);
-    builder.CreateStore(double_left, builder.CreatePointerCast(row_arg, Type::getInt64PtrTy(ctx)));
-    builder.CreateRet(double_left);
+    int64_t pos = 0;
+    Value* left_ptr_addr = builder.CreatePtrToInt(row_arg, Type::getInt64Ty(ctx));
+    Value* right_ptr_addr = builder.CreatePtrToInt(torow_arg, Type::getInt64Ty(ctx));
+    for (uint32_t i = 0; i < types.size(); ++i) {
+        int64_t len;
+        Type* type;
+        Type* ptr_type;
+        bool isfloat = false;
+        switch (types[i]) {
+            case UINT:
+            case INT:
+                type = Type::getInt32Ty(ctx);
+                ptr_type = Type::getInt32PtrTy(ctx);
+                len = 4;
+                break;
+            case ULONG:
+            case LONG:
+                type = Type::getInt64Ty(ctx);
+                ptr_type = Type::getInt64PtrTy(ctx);
+                len = 8;
+                break;
+            case FLOAT:
+                type = Type::getFloatTy(ctx);
+                ptr_type = Type::getFloatPtrTy(ctx);
+                len = sizeof(float);
+                isfloat = true;
+                break;
+
+            case DOUBLE:
+                type = Type::getDoubleTy(ctx);
+                ptr_type = Type::getDoublePtrTy(ctx);
+                len = sizeof(double);
+                isfloat = true;
+                break;
+        }
+        Value* pos_value = ConstantInt::get(Type::getInt64Ty(ctx), pos);
+        Value* left_ptr_int = builder.CreateAdd(pos_value, left_ptr_addr); 
+        Value* left_ptr = builder.CreateIntToPtr(left_ptr_int, ptr_type);
+        Value* left = builder.CreateLoad(left_ptr);
+        Value* right_ptr_int = builder.CreateAdd(pos_value, right_ptr_addr); ;
+        Value* right_ptr = builder.CreateIntToPtr(right_ptr_int, ptr_type);
+        Value* right = builder.CreateLoad(right_ptr);
+
+        Value* sum ;
+        if (isfloat) {
+            sum =  builder.CreateFAdd(left, right);
+        } else {
+            sum = builder.CreateAdd(left, right);
+        } 
+        builder.CreateStore(sum, right_ptr);
+        pos += len;
+    }
+
+    builder.CreateRet(ConstantInt::get(Type::getInt64Ty(ctx), pos));
     return fn;
 }
 
 
-
+struct test_type {
+    double double_v;
+    uint64_t uint64_v;
+    int64_t int64_v;
+    int32_t int32_v;
+    uint32_t uint32_v;
+    float float_v;
+};
+void addAgg(char* row, char* rowto, std::vector<type_t>& types) {
+    int pos = 0;
+    for (uint32_t i = 0; i < types.size(); ++i) {
+        int len;
+        switch(types[i]) {
+            case ULONG:
+            case LONG:
+                len = 8;
+                *(int64_t*)(rowto + pos) += *(int64_t*)(row + pos);
+                break;
+            case UINT:
+            case INT:
+                len = 4;
+                *(int32_t*)(rowto + pos) += *(int32_t*)(row + pos);
+                break;
+            case FLOAT:
+                len = 4;
+                *(float*)(rowto + pos) += *(float*)(row + pos);
+                break;
+            case DOUBLE:
+                len = 8;
+                *(double*)(rowto + pos) += *(double*)(row + pos);
+                break;
+        }
+        pos += len;
+    }
+}
 int main(int argc, char **argv) {
-  int n = argc > 1 ? atol(argv[1]) : 24;
-
+  int n = argc > 1 ? atol(argv[1]) : 1000000;
+  std::vector<type_t> types;
+  types.push_back(DOUBLE);
+  types.push_back(ULONG);
+  types.push_back(LONG);
+  types.push_back(INT);
+  types.push_back(UINT);
+  types.push_back(FLOAT);
   InitializeNativeTarget();
   InitializeNativeTargetAsmPrinter();
+
   LLVMContext Context;
 
   // Create some module to put our function into it.
@@ -74,7 +185,10 @@ int main(int argc, char **argv) {
 
   // We are about to create the "fib" function:
   //Function *FibF = CreateFibFunction(M, Context);
-  Function *function = codegenAggr(M, Context);
+  std::chrono::time_point<std::chrono::system_clock> start, end;
+  std::chrono::duration<double> elapsed_seconds = end-start;
+  start = std::chrono::system_clock::now();
+  Function *function = codegenAggr(M, Context, types);
 
   // Now we going to create JIT
   std::string errStr;
@@ -84,6 +198,16 @@ int main(int argc, char **argv) {
     .setEngineKind(EngineKind::JIT)
     .setErrorStr(&errStr)
     .create();
+
+  legacy::FunctionPassManager OurFPM(M);
+  M->setDataLayout(EE->getDataLayout());
+  OurFPM.add(createBasicAliasAnalysisPass());
+  OurFPM.add(createInstructionCombiningPass());
+  OurFPM.add(createReassociatePass());
+  OurFPM.add(createGVNPass());
+  OurFPM.add(createCFGSimplificationPass());
+  OurFPM.doInitialization();
+  OurFPM.run(*function);
 
   if (!EE) {
     errs() << argv[0] << ": Failed to construct ExecutionEngine: " << errStr
@@ -96,20 +220,71 @@ int main(int argc, char **argv) {
   //  errs() << argv[0] << ": Error constructing function!\n";
   //  return 1;
   //}
+  end = std::chrono::system_clock::now();
+  elapsed_seconds = end-start;
+  std::cout << "codegen elapsed time: " << elapsed_seconds.count() << "s\n";
   EE->finalizeObject();
 
   errs() << "OK\n";
-  errs() << "We just constructed this LLVM module:\n\n---------\n" << *M;
-  errs() << "---------\nstarting fibonacci(" << n << ") with JIT...\n";
+  //errs() << "We just constructed this LLVM module:\n\n---------\n" << *M;
+  errs() << "---------\nstarting aggr(" << n << ") with JIT...\n";
   typedef uint64_t (*PFN) (char* row, char* rowto);
   PFN pfe = reinterpret_cast<PFN>(EE->getPointerToFunction(function));
-  char row[16], rowto[16];
-  *(uint64_t*)row = 100;
-  *(uint64_t*)(row+8) = 200;
-  *(uint64_t*)rowto = 100;
-  *(uint64_t*)(rowto+8) = 200;
-  uint64_t rt = pfe(row, rowto);
+  end = std::chrono::system_clock::now();
+  elapsed_seconds = end-start;
+  std::cout << "codegen elapsed time: " << elapsed_seconds.count() << "s\n";
+  char row[36], rowto[36];
+  memset(rowto, 0, 36);
+  struct test_type* test_v;
+  test_v = (struct test_type*) row;
+  test_v->uint64_v = 1;
+  test_v->int64_v = 2;
+  test_v->double_v = 3.0;
+  test_v->uint32_v = 4;
+  test_v->int32_v = 5;
+  test_v->float_v = 6.0;
+  std::cout << "row:" << endl;
+  std::cout << test_v->uint64_v << endl;
+  std::cout << test_v->int64_v << endl;
+  std::cout << test_v->double_v<< endl;
+  std::cout << test_v->int32_v << endl;
+  std::cout << test_v->uint32_v << endl;
+  std::cout << test_v->float_v << endl;
+  uint64_t rt;
+  start = std::chrono::system_clock::now();
+  
+  for (int i = 0; i < n; ++i) {
+    rt = pfe(row, rowto);
+  }
+  //start = std::chrono::system_clock::now();
+  end = std::chrono::system_clock::now();
+  elapsed_seconds = end-start;
+  std::cout << "codegen calc elapsed time: " << elapsed_seconds.count() << "s\n";
   outs() << rt << "\n";
+  test_v = (struct test_type*) rowto;
+  std::cout << "result:" << endl;
+  std::cout << test_v->uint64_v << endl;
+  std::cout << test_v->int64_v << endl;
+  std::cout << test_v->double_v<< endl;
+  std::cout << test_v->uint32_v << endl;
+  std::cout << test_v->int32_v << endl;
+  std::cout << test_v->float_v << endl;
+  memset(rowto, 0, 36);
+  start = std::chrono::system_clock::now();
+  for (int i = 0; i < n; ++i) {
+    addAgg(row, rowto, types);
+  }
+  end = std::chrono::system_clock::now();
+  elapsed_seconds = end-start;
+  std::cout << "calc elapsed time: " << elapsed_seconds.count() << "s\n";
+  test_v = (struct test_type*) rowto;
+  std::cout << "result:" << endl;
+  std::cout << test_v->uint64_v << endl;
+  std::cout << test_v->int64_v << endl;
+  std::cout << test_v->double_v<< endl;
+  std::cout << test_v->uint32_v << endl;
+  std::cout << test_v->int32_v << endl;
+  std::cout << test_v->float_v << endl;
 
   // Call the Fibonacci function with argument n:
   //std::vector<GenericValue> Args(1);
